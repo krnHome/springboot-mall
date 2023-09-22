662473ef1fd879fe511d19cb8cd9182e7ceee194
krn0829 <krn0829@gmail.com>
2023-09-22 13:21:54 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- springboot-mall/src/main/java/com/kujudy/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/kujudy/springbootmall/dao/ProductDao.java
@@ -3,7 +3,10 @@ package com.kujudy.springbootmall.dao;
 import com.kujudy.springbootmall.dto.ProductRequest;
 import com.kujudy.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId,ProductRequest productRequest);
--- springboot-mall/src/main/java/com/kujudy/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/kujudy/springbootmall/service/ProductService.java
@@ -3,11 +3,15 @@ package com.kujudy.springbootmall.service;
 import com.kujudy.springbootmall.dto.ProductRequest;
 import com.kujudy.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
 
     void updateProduct(Integer productId, ProductRequest productRequest);
     void deleteProductById(Integer productId);
+
 }
