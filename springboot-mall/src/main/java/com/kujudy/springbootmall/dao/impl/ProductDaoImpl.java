50a4f6b60bd2d2f21496f71fa206b1f4b4055542
krn0829 <krn0829@gmail.com>
2023-09-23 09:15:56 +0800

    商品功能 - 提煉程式

--- springboot-mall/src/main/java/com/kujudy/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/kujudy/springbootmall/dao/impl/ProductDaoImpl.java
@@ -1,6 +1,5 @@
 package com.kujudy.springbootmall.dao.impl;
 
-import com.kujudy.springbootmall.constant.ProductCategory;
 import com.kujudy.springbootmall.dao.ProductDao;
 import com.kujudy.springbootmall.dto.ProductQueryParams;
 import com.kujudy.springbootmall.dto.ProductRequest;
@@ -31,15 +30,7 @@ public class ProductDaoImpl implements ProductDao {
         Map<String,Object> map = new HashMap<>();
 
         //查詢條件
-        if(productQueryParams.getCategory() != null){
-            sql= sql + " and category = :category";
-            map.put("category",productQueryParams.getCategory().name());
-        }
-
-        if(productQueryParams.getSearch() != null){
-            sql = sql + " and product_name like :search";
-            map.put("search", "%" + productQueryParams.getSearch() + "%");
-        }
+        sql = addFilteringSql(sql,map,productQueryParams);
 
       Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
         return total;
@@ -52,15 +43,7 @@ public class ProductDaoImpl implements ProductDao {
         Map<String,Object> map = new HashMap<>();
 
         //查詢條件
-        if(productQueryParams.getCategory() != null){
-            sql= sql + " and category = :category";
-            map.put("category",productQueryParams.getCategory().name());
-        }
-
-        if(productQueryParams.getSearch() != null){
-            sql = sql + " and product_name like :search";
-            map.put("search", "%" + productQueryParams.getSearch() + "%");
-        }
+        sql = addFilteringSql(sql,map,productQueryParams);
 
         //排序
         sql = sql + " order by " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
@@ -147,6 +130,20 @@ public class ProductDaoImpl implements ProductDao {
 
         namedParameterJdbcTemplate.update(sql,map);
 
+    }
+
+    private String addFilteringSql(String sql,Map<String, Object> map,ProductQueryParams productQueryParams){
+        //查詢條件
+        if(productQueryParams.getCategory() != null){
+            sql= sql + " and category = :category";
+            map.put("category",productQueryParams.getCategory().name());
+        }
+
+        if(productQueryParams.getSearch() != null){
+            sql = sql + " and product_name like :search";
+            map.put("search", "%" + productQueryParams.getSearch() + "%");
+        }
+        return sql;
     }
 
 
